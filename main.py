eb679029d337f3e092f9df169d191347161d98ed
Kroko-fant <schrottsuperdeluxe@gmail.com>
2021-03-13 22:59:13 +0100

    added load, reload, unload command

--- main.py
+++ main.py
@@ -4,25 +4,58 @@ from discord.ext import commands
 from discord import Intents
 
 
+# Returns a prefix. Warning: Two Arguments required
 def get_prefix(client, message):
     return "py!"
 
 
+# A Function to load all Modules
+def load_modules():
+    for file in os.listdir('./cogs'):
+        if file.endswith(".py"):
+            client.load_extension(f'cogs.{file[:-3]}')
+            print(f"Starting {file}")
+
+
+# Creates a client
 client = customclient.CustomClient(command_prefix=get_prefix, intents=Intents.all(), case_insensitive=True)
+load_modules()
 
 
+# Shows Ping
 @client.command()
 async def ping(ctx):
-    """Zeigt den aktuellen Ping"""
-    await ctx.send( f'Pong!')
+    """Shows the actual Ping"""
+    await ctx.send(f'Pong!')
+
+
+# Load a module
+@client.command()
+async def load(ctx, extension):
+    """Loads a module"""
+    await ctx.send(f':green_circle: {extension} loaded!')
+
+
+# unload a module
+@client.command()
+async def unload(ctx, extension):
+    """unloads a module"""
+    await ctx.send(f':green_circle: {extension} unloaded!')
+
+
+# Reload a module
+@client.command()
+async def reload(ctx, extension):
+    """Reloads a module"""
+    await ctx.send(f':green_circle: {extension} reloaded!')
 
 
 # Load Modules
 @client.command()
 @commands.is_owner()
-async def shutdown(ctx, reason="erwarteter Shutdown"):
-    """Fährt den Bot herunter. Danach muss man ihn auf dem Server in der Console neustarten lol."""
-    await ctx.send("Bot wird heruntergefahren...")
+async def shutdown(ctx, reason="regular shutdown"):
+    """Shuts the bot down"""
+    await ctx.send("Bot shuts down...")
     client.reason = reason
     await client.close()
 
