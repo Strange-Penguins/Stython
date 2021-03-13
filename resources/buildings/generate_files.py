f28339965b5c342288e82897d844f9a88c8fc11e
Simolater <s.giesse@gmx.net>
2021-03-14 00:46:55 +0100

    Added building json files

---
+++ resources/buildings/generate_files.py
@@ -0,0 +1,4 @@
+buildings = ["HEADQUARTER", "BARRACKS", "STABLE", "WORKSHOP", "ACADEMY", "SMITHY", "RALLY_POINT", "STATUE", "MARKET", "TIMBER_CAMP", "CLAY_PIT", "IRON_MINE", "FARM", "WAREHOUSE", "HIDING_PLACE", "WALL"]
+
+for name in buildings:
+    f = open(f"{name}.json", "w")
