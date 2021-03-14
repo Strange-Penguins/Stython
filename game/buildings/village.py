82b2ca1ce9dd27c9a7275fb6d5beec62935d2f1a
Simolater <s.giesse@gmx.net>
2021-03-14 15:29:36 +0100

    Village base

--- game/buildings/village.py
+++ game/buildings/village.py
@@ -1,16 +1,15 @@
-# Village Headquarters
-# Barracks
-# Stable
-# Workshop
-# Academy
-# Smithy
-# Rally Point
-# Statue
-# Market
-# Timber camp
-# Clay pit
-# Iron mine
-# Farm
-# Warehouse
-# Hiding place
-# Wall
+import time
+
+
+class Village:
+
+    def __init__(self, game_data, pos=(0, 0)):
+        self.position = pos
+        self.buildings = [0] * len(game_data.buildings_list)
+        self.last_update = time.monotonic_ns() / 1_000_000
+
+    def update(self, game_data):
+        current_time = time.monotonic_ns() / 1_000_000
+        time_diff = current_time - self.last_update
+        self.last_update = current_time
+
