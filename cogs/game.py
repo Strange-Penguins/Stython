98e6f5a8ecaecd480283739817d01eeb87ea1ea1
Kroko-fant <schrottsuperdeluxe@gmail.com>
2021-03-14 12:57:19 +0100

    added a template for an overview and added map as a todo

--- cogs/game.py
+++ cogs/game.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 
@@ -12,15 +13,72 @@ class Game(commands.Cog):
     # Switch the World the player is currently playing on
     @commands.command()
     async def switchworld(self, ctx):
-        await ctx.send("Not implemented")
-
-    @commands.command()
-    async def attack(self, ctx):
+        """Switches the current active world of a player"""
         await ctx.send("Not implemented")
 
     @commands.command()
     async def overview(self, ctx):
+        """Shows an overview of your current village"""
+        # TODO: Points calculation
+        points = 26
+
+        points_path = {10999: "https://cdn.discordapp.com/attachments/820622377208643586/820622658370142278/W6.png",
+                       8999: "https://cdn.discordapp.com/attachments/820622377208643586/820622636794118164/W5.png",
+                       2999: "https://cdn.discordapp.com/attachments/820622377208643586/820622582789308446/W4.png",
+                       999: "https://cdn.discordapp.com/attachments/820622377208643586/820622556511469568/W3.png",
+                       299: "https://cdn.discordapp.com/attachments/820622377208643586/820622462009475082/W2.png"}
+
+        overview_embed = discord.Embed(color=ctx.author.color, timestamp=ctx.message.created_at,
+                                       title=f"Village of {ctx.author}"
+                                       )
+
+        for k, v in points_path.items():
+            if points > k:
+                overview_embed.set_thumbnail(
+                    url=v)
+                break
+        else:
+            overview_embed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/820622377208643586/820622462005149696/W1.png")
+        overview_embed.add_field(name="Resources",
+                                 value=f":wood: {0}/{0} :bricks: {0}/{0} :nut_and_bolt: {0}/{0} "
+                                       f":bust_in_silhouette: {0}/{0}"
+                                 )
+
+        overview_embed.add_field(name="Punkte", value=f'{points}')
+
+        overview_embed.add_field(name="Buildings",
+                                 value=f":office: {0} :crossed_swords: {0} :horse: {0} :hammer_pick: {0} "
+                                       f":european_castle: {0} :hammer: {0} :bow_and_arrow: {0} :statue_of_liberty:"
+                                       f"{0}\n:department_store: {0} :wood: {0} :bricks: {0} :nut_and_bolt:  {0} "
+                                       f":farmer: {0} :classical_building:  {0} :key: {0} :shield: {0}",
+                                 inline=False
+                                 )
+
+        overview_embed.add_field(name="Troops",
+                                 value=f":magic_wand: {0} :crossed_swords: {0} :axe: {0} :mag: {0} :horse_racing: {0}\n"
+                                       f":carousel_horse: {0} :left_facing_fist: {0} :comet: {0} :dagger: {0}"
+                                       f":crown: {0}",
+                                 inline=False
+                                 )
+
+        await ctx.send(embed=overview_embed)
+
+        # Add emojis
+
+        # Register Listener
+
+    @commands.command()
+    async def map(self, ctx):
+        """Shows the map"""
         await ctx.send("Not implemented")
+
+    @commands.command()
+    async def attack(self, ctx):
+        """Attacks a village"""
+        await ctx.send("Not implemented")
+
+    # @commands.Cog.listener(): TODO: Do a eventlistener
 
 
 def setup(client):
